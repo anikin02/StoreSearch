456e37852922c506d0157f78ab7e0ea920aa65bb
Danil Anikin <mart4r.clearlist@gmail.com>
2023-10-09 15:12:57 +1000

    Initial Commit

---
+++ StoreSearch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StoreSearch
+//
+//  Created by Данил Аникин on 09.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
