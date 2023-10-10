17824af88b26df5a9b4d0a419d0106420738b311
Danil Anikin <mart4r.clearlist@gmail.com>
2023-10-11 09:43:18 +1000

    Organized files

---
+++ StoreSearch/View Controllers/SearchViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  StoreSearch
+//
+//  Created by anikin02 on 09.10.2023.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+
+
+}
+
