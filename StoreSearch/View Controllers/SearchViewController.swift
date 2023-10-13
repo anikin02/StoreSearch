56f34e7d50289d9ad73c04b1a7b96c632702a137
Danil Anikin <mart4r.clearlist@gmail.com>
2023-10-13 15:53:55 +1000

    Added delegates

--- StoreSearch/View Controllers/SearchViewController.swift
+++ StoreSearch/View Controllers/SearchViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  SearchViewController.swift
 //  StoreSearch
 //
 //  Created by anikin02 on 09.10.2023.
@@ -8,11 +8,31 @@
 import UIKit
 
 class SearchViewController: UIViewController {
-
+  @IBOutlet weak var searchBar: UISearchBar!
+  @IBOutlet weak var tableView: UITableView!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
   }
-
-
 }
+
+// MARK: - Search Bar Delegate extension
+
+extension SearchViewController: UISearchBarDelegate {
+  func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+    print("The search text is: '\(searchBar.text!)'") }
+}
+
+// MARK: - Table View Delegate extension
+
+extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 0
+  }
+
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    return UITableViewCell()
+  }
+}
+
 
