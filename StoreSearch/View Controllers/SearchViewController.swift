5695f5e0ab5009397d6fb949e07c25073842c5ec
Danil Anikin <mart4r.clearlist@gmail.com>
2023-10-14 21:44:38 +1000

    Added keyboard closure after clicking on search

--- StoreSearch/View Controllers/SearchViewController.swift
+++ StoreSearch/View Controllers/SearchViewController.swift
@@ -11,8 +11,12 @@ class SearchViewController: UIViewController {
   @IBOutlet weak var searchBar: UISearchBar!
   @IBOutlet weak var tableView: UITableView!
   
+  var searchResults = [String]()
+  
   override func viewDidLoad() {
     super.viewDidLoad()
+    
+    tableView.contentInset = UIEdgeInsets(top: 51, left: 0, bottom: 0, right: 0)
   }
 }
 
@@ -20,18 +24,29 @@ class SearchViewController: UIViewController {
 
 extension SearchViewController: UISearchBarDelegate {
   func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-    print("The search text is: '\(searchBar.text!)'") }
+    for i in 0...2 { searchResults.append( String( format: "Fake Result %d for '%@'", i, searchBar.text! ) ) }
+    tableView.reloadData()
+    searchBar.resignFirstResponder()
+  }
 }
 
 // MARK: - Table View Delegate extension
 
 extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
   func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return 0
+    return searchResults.count
   }
 
   func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    return UITableViewCell()
+    let cellIdentifier = "SearchResultCell"
+    var cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier)
+    
+    if cell == nil {
+      cell = UITableViewCell(style: .default, reuseIdentifier: cellIdentifier)
+    }
+    
+    cell!.textLabel!.text = searchResults[indexPath.row]
+    return cell!
   }
 }
 
